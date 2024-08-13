cbf37e9e5efcff7a6fce6e8d38b01c17215f74db
GunKate <gokhfeld.ev@gmail.com>
2024-08-13 21:47:40 +0500

    upp error

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 7406240192:AAEQc8XoF1xHtAyVWVWGaaBguka-YCzTvDE
-api_key = "35e5c447482930f3a11caef74c20293c"
+TOKEN = '7406240192:AAEQc8XoF1xHtAyVWVWGaaBguka-YCzTvDE'
+api_key = '35e5c447482930f3a11caef74c20293c'
 city = 'Ekaterinburg'
